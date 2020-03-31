21670f8e929b78c738e5d527664d41db74fce3cb
Elliott <itali43@gmail.com>
2020-03-30 18:33:12 -0700

    Initial Commit

---
+++ TicTacInfinity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacInfinity
+//
+//  Created by Elliott Williams on 3/30/20.
+//  Copyright © 2020 Moonshot Applications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
